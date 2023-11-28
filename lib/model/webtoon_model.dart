e51d5c9dc13ebe22512509a60cf21f5c1a5c53df
shinseung kim <94797657+kimshinseung@users.noreply.github.com>
2023-11-28 17:41:49 +0900

    recap

--- lib/model/webtoon_model.dart
+++ lib/model/webtoon_model.dart
@@ -6,4 +6,11 @@ class WebtoonModel {
   title = json['title'],
   thumb = json['thumb'],
   id = json['id'];
+
+  //위랑 같다( 변수에 late)
+  // WebtoonModel(Map<String,dynamic> json){
+  //   title = json['title'];
+  //   thumb = json['thumb'];
+  //   id = json['id'];
+  // }
 }
